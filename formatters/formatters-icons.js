decd378cc50409a701e36ee3d137c0e49d9ab92c
marlyk <leemark0071@gmail.com>
2020-03-24 20:31:32 +0000

    Update formatters-icons.js

--- formatters/formatters-icons.js
+++ formatters/formatters-icons.js
@@ -21,5 +21,29 @@ Tabulator.prototype.extendModule("format", "formatters", {
             Object.keys(formatterParams.tags).forEach(function(key){
                 iconHolder.setAttribute(key, formatterParams.tags[key])})}
         return iconHolder;
+    },
+
+    iconMapPivot: function(cell, formatterParams){
+        const pivotVal = cell.getRow().getData()[formatterParams.pivot];
+
+        const iconHolder = document.createElement("i");
+        iconHolder.className = cell.getValue();
+
+        // Define the icon
+        if (pivotVal in formatterParams.iconMapping){
+            iconHolder.className = formatterParams.iconMapping[pivotVal]; }
+
+        // Set specific CSS mappings if exists
+        if (typeof formatterParams.cssMapping !== 'undefined'){
+            if (pivotVal in formatterParams.cssMapping){
+                Object.keys(formatterParams.cssMapping[pivotVal]).forEach(function(key){
+                    const.style[key] = formatterParams.cssMapping[pivotVal][key]})
+            }
+        }
+        // Add some CSS tags to the icon holder
+        if (typeof formatterParams.tags !== 'undefined'){
+            Object.keys(formatterParams.tags).forEach(function(key){
+                iconHolder.setAttribute(key, formatterParams.tags[key])})}
+        return iconHolder;
     }
 });
